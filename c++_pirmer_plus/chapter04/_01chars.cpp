dfa1f5a71cd92f7f3b6b461825b39d8a1bd6aac9
root <root@localhost.localdomain>
2017-02-08 13:59:08 +0800

    chars \0

--- c++_pirmer_plus/chapter04/_01chars.cpp
+++ c++_pirmer_plus/chapter04/_01chars.cpp
@@ -5,8 +5,11 @@
  *      Author: ludonghua
  */
 #include <iostream>
+using namespace std;
 
 int main(){
 	char str[] = "abcdefg";
-	cout << sizeof(str) << endl;
+	cout << sizeof(str) << " : " << str << endl;
+    str[2] = '\0';
+    cout << str << endl;
 }
